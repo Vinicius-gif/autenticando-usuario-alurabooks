c55dad7d10eef76432375a476d3a2c0ef76ca975
Vinicius-gif <vfpv@hotmail.com>
2023-03-19 23:26:35 -0300

    ...

--- src/paginas/PaginaBase/index.tsx
+++ src/paginas/PaginaBase/index.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom"
 import BarraNavegacao from "../../componentes/BarraNavegacao"
-import Newsletter from "../../componentes/Newsletter"
 import Rodape from "../../componentes/Rodape"
-import TagsCategorias from "../../componentes/TagsCategorias"
 
 const PaginaBase = () => {
     return (<main>
         <BarraNavegacao />
         <Outlet />
-        <TagsCategorias />
-        <Newsletter />
         <Rodape />
     </main>)
 }
